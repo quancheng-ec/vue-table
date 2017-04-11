5046c43afd3cf4bc3a6b72bb3ecd89287f97b03e
焦哥 <jiaoyingjun@quancheng-ec.com>
2017-04-11 17:42:48 +0800

    fix

---
+++ components/ImageText.vue
@@ -0,0 +1,33 @@
+<template>
+  <span>
+        <img :src="it.param.url" :style="style" />{{ it.value }}
+      </span>
+</template>
+
+<script>
+export default {
+  // 接收上层传递的value
+  props: ['it'],
+  data() {
+    return {
+      style: ''
+    }
+  },
+  created() {
+    if (this.it.param.width) {
+      this.style += 'width:' + this.it.param.width + ";";
+    }
+    if (this.it.param.height) {
+      this.style += 'height:' + this.it.param.height + ";";
+    }
+  },
+  methods: {
+  }
+
+}
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
