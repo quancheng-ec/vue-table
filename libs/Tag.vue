fb2b1abc7321c4abfdb8610f5ac8cbcda470dca0
焦哥 <jiaoyingjun@quancheng-ec.com>
2017-04-24 22:22:53 +0800

    fix

---
+++ libs/Tag.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <template v-if="item[it.name]">
+      <span class="tag label label-default text-muted qc-label"
+            v-for="(itt,index) in item[it.name]" v-if="itt.label">{{itt.label}}
+       </span>
+    </template>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['it', 'item'],
+  data() {
+    return {
+    }
+  },
+  methods: {
+
+  },
+  components: {
+  }
+
+}
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
